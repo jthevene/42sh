2a021e915d0a131fba16da3f65ab93bc6fd8fd47
Mindc0re <simongaudin65@gmail.com>
2017-01-30 15:55:19 +0100

    Backslash et quotes sont gerees par le verif_token maintenant

--- globing/verif_tokens.c
+++ globing/verif_tokens.c
@@ -12,6 +12,30 @@
 
 #include "../includes/globing.h"
 
+int					ft_strchr_quotes(char *s, int c)
+{
+	FT_INIT(int, i, 0);
+	FT_INIT(int, quote, 0);
+	FT_INIT(int, found, 0);
+	while (s[i] && i < (int)ft_strlen(s))
+	{
+		if (s[i] == '\'' || s[i] == '"')
+		{
+			quote = s[i++];
+			while (s[i] && s[i] != quote)
+			{
+				if ((c == '{' || c == '}') && s[i] == ',')
+					return (0);
+				i++;
+			}
+		}
+		if (s[i] == c)
+			found++;
+		i++;
+	}
+	return (found);
+}
+
 int					ft_strchr_bslash(char *s, int c)
 {
 	FT_INIT(int, i, 0);
@@ -28,6 +52,8 @@ int					ft_strchr_bslash(char *s, int c)
 			found++;
 		i++;
 	}
+	if (ft_strchr(s, '\'') || ft_strchr(s, '"'))
+		return (ft_strchr_quotes(s, c));
 	return (found);
 }
 
@@ -36,17 +62,24 @@ int					verif_tokens(char *str)
 	FT_INIT(int, nb, 0);
 	if (ft_strchr_bslash(str, '[') && !ft_strchr_bslash(str, ']'))
 		return (0);
-//	else if (!count_brackets(str, '['))
-//		return (0); 
+	else if (!ft_strchr_bslash(str, '[') && ft_strchr_bslash(str, ']'))
+		return (0);
+	else if (!count_brackets(str, '['))
+		return (0); 
 	else if (ft_strchr_bslash(str, '[') && ft_strchr_bslash(str, ']'))
 		nb++;
 	if (ft_strchr_bslash(str, '{') && !ft_strchr_bslash(str, '}'))
+		return (0);
+	else if (ft_strchr_bslash(str, '{') && !ft_strchr_bslash(str, '}'))
 		return (0);
 	else if (!count_brackets(str, '{'))
 		return (0);
 	else if (ft_strchr_bslash(str, '{') && ft_strchr_bslash(str, '}'))
 		nb++;
-	if (ft_strchr_bslash(str, '*') || ft_strchr_bslash(str, '?'))
+	if ((ft_strchr(str, '*') && !ft_strchr_bslash(str, '*'))
+		|| (ft_strchr(str, '?') && !ft_strchr_bslash(str, '?')))
+		return (0);
+	else if (ft_strchr_bslash(str, '*') || ft_strchr_bslash(str, '?'))
 		nb++;
 	if (!nb)
 		return (0);

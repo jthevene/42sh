a69ab4095963fd619c5067565135a0a97c60ec75
dieuson <dieuson.v@gmail.com>
2017-02-23 19:53:48 +0100

    error exe_function OK

--- execution/exec_function.c
+++ execution/exec_function.c
@@ -28,6 +28,14 @@ int				exec_function_execve(char *cmd, char **args)
 	}
 }
 
+int 			error_parse_bin(char *error)
+{
+	ft_putstr_fd("21sh:", 2);
+	ft_putstr_fd(error, 2);
+	ft_putstr_fd(":No such file or directory\n", 2);
+	return (0);
+}
+
 int				parse_bin_directories(char **bin_dir, char **args)
 {
 	FT_INIT(int, i, 0);
@@ -38,7 +46,7 @@ int				parse_bin_directories(char **bin_dir, char **args)
 		if (verif_access_bin(args[0]))
 			exec_function_execve(args[0], args);
 		else
-			return (ft_printf("21sh:%s:No such file or directory\n", args[0]));
+			return (error_parse_bin(args[0]));
 	}
 	while (bin_dir && bin_dir[i])
 	{

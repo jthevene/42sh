1bf4e967789764ae748259b1de271d09cd278b63
Mindc0re <simongaudin65@gmail.com>
2017-02-24 20:48:59 +0100

    remis les segfault

--- line_edition/signal.c
+++ line_edition/signal.c
@@ -6,7 +6,7 @@
 /*   By: sgaudin <sgaudin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/24 18:13:29 by apinho            #+#    #+#             */
-/*   Updated: 2017/02/24 20:14:37 by sgaudin          ###   ########.fr       */
+/*   Updated: 2017/02/24 20:47:44 by sgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,12 +44,22 @@ void		ft_sigkill(int sig)
 	ft_exit();
 }
 
+void		ft_segfault(int sig)		
+{		
+	(void)sig;		
+	ft_putstr("Error segfault\nFin du programme\n");		
+	ft_reset_termios(g_shell.t_back);		
+	ft_exit();		
+}
+
 void		distrib_signals(int sig)
 {
 	if (sig == SIGWINCH)
 		ft_sigwinch(sig);
 	else if (sig == SIGINT)
 		ft_sigint(sig);
+	else if (sig == SIGSEGV)
+		ft_segfault(sig);
 }
 
 void		ft_signal(void)

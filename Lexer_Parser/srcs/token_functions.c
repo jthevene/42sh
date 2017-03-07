459ef84e85f0f93d3e66d470e45bb0202171f1be
LeManPinho <moonsheik@gmail.com>
2017-03-07 21:04:48 +0100

    plop

--- Lexer_Parser/srcs/token_functions.c
+++ Lexer_Parser/srcs/token_functions.c
@@ -20,6 +20,8 @@ int		token_squote(t_token *token, char *line, int pos, int i)
 	while (line[pos] != '\'')
 		pos++;
 	pos++;
+	while (ft_isprintnotope(line[pos]) == 1)
+		pos++;
 	add_lexeme(token, line, pos, i);
 	return (pos);
 }
@@ -32,6 +34,8 @@ int		token_dquote(t_token *token, char *line, int pos, int i)
 	while (line[pos] != '\"')
 		pos++;
 	pos++;
+	while (ft_isprintnotope(line[pos]) == 1)
+		pos++;
 	add_lexeme(token, line, pos, i);
 	return (pos);
 }
